dcbaf7903cd4cd2960ed4c72272661010485b148
nikitachhillar <69665892+nikitachhillar@users.noreply.github.com>
2021-11-15 15:13:43 +0530

    Update urls.py

--- pdfbuilder/urls.py
+++ pdfbuilder/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',views.index,name="index"),
+    path('',views.index,name="index"),
     path('result/',views.result,name="result"),
 ]
